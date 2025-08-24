db64a4f1ec54a274c824154d74b2114c1bcbbe2d
fabiofiori <fiorifabio1210@gmail.com>
2025-08-24 15:57:21 +0200

    moved TaskFormDialog to dialogs folder

--- src/components/tasks/index.js
+++ src/components/tasks/index.js
@@ -1,4 +1,4 @@
 export { default as TaskTable } from './TaskTable'
 export { default as TaskFilters } from './TaskFilters'
 export { default as TaskSidebar } from './TaskSidebar'
-export { default as TaskFormDialog } from './TaskFormDialog'
+export { default as TaskFormDialog } from '../dialogs/TaskFormDialog'
